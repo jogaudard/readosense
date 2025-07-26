dbf7300ee8f8fe9b41634e524c04881fe6879b98
jogaudard <joseph.gaudard@pm.me>
2025-07-26 17:03:23 +0200

    added full dev workflow

--- building_material/dev.R
+++ building_material/dev.R
@@ -3,3 +3,36 @@ available::available("readosense")
 
 package_path <- "/home/jga051/Documents/01_PhD/06_projects/readosense"
 usethis::create_package(path = package_path)
+
+usethis::use_test(name = "")
+
+# build the documentation
+devtools::document()
+
+# check the examples work
+devtools::run_examples()
+
+# check the tests work
+devtools::test()
+
+
+usethis::use_package("")
+
+devtools::check()
+
+lintr::lint_package()
+
+devtools::load_all()
+
+# to make the webpage
+usethis::use_pkgdown()
+pkgdown::build_site()
+usethis::use_pkgdown_github_pages()
+
+usethis::use_readme_rmd()
+devtools::build_readme()
+
+usethis::use_vignette("licoread")
+devtools::build_vignettes()
+
+rcompendium::add_lifecycle_badge(lifecycle = "experimental", quiet = FALSE)
