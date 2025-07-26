b555bfcf8cd1c1c450ec87b4f0d60f63bc9f1860
jogaudard <joseph.gaudard@pm.me>
2025-07-26 20:05:19 +0200

    eo env works

--- R/eo_env.R
+++ R/eo_env.R
@@ -4,15 +4,21 @@
 #' pressure (atm) for all chambers
 #' @importFrom dplyr mutate select
 #' @export
-#' @examples 
+#' @examples
+#' path <- system.file("extdata/ex_log", package = "readosense")
+#' eo_import_logs(path) |>
+#' eo_env()
 
 
 eo_env <- function(logs) {
 
-chamber_temp_pres <- chamber_log_all |>
+  chamber_temp_pres <- logs |>
     mutate(
-        air_temp = temperaturev * 15, # need to ask the exact conversion factor, for now this one makes sense
-        pressure = pressure / 101.325 # need atm for fluxible
+      # need to ask the exact conversion factor, for now this one makes sense
+      air_temp = .data$temperaturev * 15,
+      pressure = .data$pressure / 101.325 # need atm for fluxible
     ) |>
-    select(datetime, air_temp, pressure)
+    select("datetime", "air_temp", "pressure")
+
+  chamber_temp_pres
 }
--- tests/testthat/test-eo_env.R
+++ tests/testthat/test-eo_env.R
@@ -1,3 +1,7 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("import logs work", {
+  expect_snapshot(
+    eo_import_logs(test_path("test_logs/")) |>
+      eo_env() |>
+      str(digits.d = 4, width = 100, strict.width = "cut")
+  )
 })
